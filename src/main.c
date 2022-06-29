dbd94ac3779871649e2dc03f8d75b577064d4b4b
ztnel <christian@leapsystems.online>
2022-06-29 06:01:51 +0100

    spi config struct

--- src/main.c
+++ src/main.c
@@ -1,3 +1,6 @@
+#include <stdio.h>
+#include <string.h>
+
 #include "mmap.h"
 #include "spi.h"
 
@@ -6,10 +9,11 @@ int main(void) {
   // configure spi device
   spi_config_t spi_config;
   memset(&spi_config, 0, sizeof(spi_config));
-  spi_config.device = 1;
+  spi_config.device = 0;
   spi_config.mode = 0x1;
-  spi_config.speed = 100000;
+  spi_config.speed = 1000;
+  printf("Size of spi_config: %i\n", sizeof(spi_config));
   spi_init((const spi_config_t *)&spi_config);
-  uint8_t tx_buffer[8];
-  spi_transact(tx_buffer);
+  const uint8_t tx_buffer[5] = { 0x48, 0x65, 0x6c, 0x6c, 0x6f };
+  spi_transact((const uint8_t *)&tx_buffer, sizeof(tx_buffer));
 }
